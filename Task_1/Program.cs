849710cce20d2764ca7249fd567350be889fb07f
Polina Kulikova <pomidorishna@gmail.com>
2023-01-11 17:33:47 +0300

    Вот бы преобразовать полученное число в массив

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,6 +4,11 @@
 // 12821 -> да
 // 23432 -> да
 
+// Console.WriteLine("Введите пятизначное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int[] array = number.преобразование в массив();
+
+
 int[] array = {1, 4, 2, 1, 2};
 // int[] array = {1, 2, 8, 2, 1};
 // int[] array = {2, 3, 4, 3, 2};
