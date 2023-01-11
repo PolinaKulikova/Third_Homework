b0d8ec15a3226463e109fd3c57b10a8c8dde5d6d
Polina Kulikova <pomidorishna@gmail.com>
2023-01-11 18:09:08 +0300

    Задача 3 готова

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введи число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int number = 3;
+// int number = 5;
+
+int count = 1;
+
+while (count <= number)
+{
+   Console.WriteLine(count*count*count);
+   count = count + 1;
+      
+}
+
+
+
